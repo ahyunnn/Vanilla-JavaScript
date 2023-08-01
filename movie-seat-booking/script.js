ea692de726abe82b535a6884f2cbe1eb8897bbb8
ahyunnn <ahyun3089@gmail.com>
2023-08-01 09:50:39 +0900

    230801 - movie seat booking - Selecting Movie & Seats from UI

---
+++ movie-seat-booking/script.js
@@ -0,0 +1,44 @@
+// ✅ 필요한 요소 변수로 저장
+// querySelector :
+// querySelectorAll
+// getElementById :
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+let ticketPrice = +movieSelect.value;
+
+// ✅ Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// ✅ Movie Select event - 영화를 누르면 해당 영화가 선택되도록 하는 함수
+movieSelect.addEventListener("click", (event) => {
+  ticketPrice = +event.target.value;
+  updateSelectedCount();
+});
+
+// ✅ Seat click event;
+// N/A 상태(not Occupied)의 좌석을 누르면 Selected로 클래스명 변경
+// container태그에서 click이벤트로 일어나는 모든 것들
+// classList :
+// contains :
+// toggle / add / remove :
+// class명을 적을 때 두개를 쓰는 건 각각의 클래스 명인건가? <div class="seat occupied"></div>
+container.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("seat") &&
+    !event.target.classList.contains("occupied")
+  ) {
+    event.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
