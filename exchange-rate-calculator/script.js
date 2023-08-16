d8fcb501c449eefabf4a0edf02f4cdf0a8b949cc
ahyunnn <ahyun3089@gmail.com>
2023-08-16 16:18:03 +0900

    230816 - exchange rate calculator - add fetch code

--- exchange-rate-calculator/script.js
+++ exchange-rate-calculator/script.js
@@ -10,17 +10,26 @@ function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  axios
-    .get(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    // .then((res) => res.json()) // json파일로 변경
-    .then((response) => {
-      // console.log(response);
-      const rate = response.data.rates[currency_two];
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json()) // json파일로 변경
+    .then((data) => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
 
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
+
+  // axios
+  // .get(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+  // .then((response) => {
+  //   const rate = response.data.rates[currency_two];
+
+  //   rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+  //   amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+  // });
 }
 
 // Event listeners
