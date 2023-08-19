98ce301b13538454e61647918c72aa34a4683bff
ahyunnn <ahyun3089@gmail.com>
2023-08-19 18:09:13 +0900

    230819 - DOM array methods - map() & complete double money btn

--- 6. DOM-array-methods/script.js
+++ 6. DOM-array-methods/script.js
@@ -1,13 +1,13 @@
 const main = document.getElementById("main");
 const addUserBtn = document.querySelector(".add-user");
-const doubleBtn = document.getElementById("double");
+const doubleBtn = document.querySelector(".double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
 
-// Fetch random user and add money : api를 통해 랜덤 이름, 자산 불러오기
+// ✅ Fetch random user and add money : api를 통해 랜덤 이름, 자산 불러오기
 async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
@@ -22,16 +22,16 @@ async function getRandomUser() {
   addData(newUser);
 }
 
-// Add new obj to data arr : 데이터 배열에 새로운 객체 추가
+// ✅ Add new obj to data arr : 데이터 배열에 새로운 객체 추가
 function addData(obj) {
   data.push(obj);
 
   updateDOM();
 }
 
-// Update DOM
+// ✅ Update DOM (forEach 메서드 사용)
+// 계속 DOM을 새롭게 갱신해주는 함수
 // 매개변수를 아래와 같이 쓰면 인자가 전달되지 않았을 때 해당 변수를 쓰겠다는 뜻!
-// Update DOM
 function updateDOM(providedData = data) {
   // Clear main div
   main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
@@ -46,11 +46,21 @@ function updateDOM(providedData = data) {
   });
 }
 
-// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+// ✅ Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
 // 돈 단위로 보일 수 있도록 수정
 function formatMoney(number) {
   return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 }
 
-// Event listeners
+// ✅ Double Money (map 메서드 사용)
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+
+  updateDOM();
+}
+
+// ✅ Event listeners
 addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
